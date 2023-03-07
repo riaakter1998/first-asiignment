ccf9838735658b3eacbb5b42b9caeccc4076b797
riaakter1998 <ria426306@gmail.com>
2023-03-08 01:22:58 +0600

    first commit

---
+++ 1stassignment.js
@@ -0,0 +1,71 @@
+
+
+// ১. Variable কি ?
+// Answer:সাধারন ভাবে যার মধ্যে কোন মান রাখা হয় তাই  variable
+
+
+// ২. Variable কিভাবে লিখতে হয় ?
+var FirstName = "Ria Akter";
+
+// ৩. string type variable কি ও কি ভাবে লিখতে হয় ?
+// Answert :   "" ডাবল কোট বা ' ' সিঙ্গেল কোটের ভিতরে যা লেখা হয় ভ্যারিয়বেলের ভ্যালু তাকে string data type বলা হয় । 
+var name = "hello";
+
+
+// ৪. number type variable কি ও কি ভাবে লিখতে হয় ?
+// Answer : ভ্যারিয়বেলের ভ্যালু হিসাবে যদি Number স্টোর করতে চায় মেমোরিতে (Ram Memory) তাহলে কোন কোট ছাড়ায় লিখতে হবে 
+var x = 22;
+
+
+// ৫. Boolan type variable  কি ও কি ভাবে লিখতে হয় ?
+// Answer:এর দুটি ভ্যালু (Value) আছে একটি True অপরটি False । 
+var x = true;
+console.log(x);
+console.log(typeof x);
+
+// ৬. toUpperCase() & toLowerCase() এর ব্যাবহার কি ভাবে করতে হয় ? 
+
+var text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has";
+console.log(text.toLowerCase());
+console.log(text.toUpperCase());
+
+
+// ৭. JavaScript এর মোট কয়টি অপারেটর আছে ও কি কি ?
+
+// Answer : variable ও অন্যা্ন্য object পারস্পরিক গাণিতিক ও যৌক্তিক সম্পর্ক বোঝানোর জন্য জাভাস্ক্রিপ্ট অপারেটর ব্যবহার করা হয়। কাজের ধরন ও ব্যবহার অনুসারে জাভাস্ক্রিপ্ট অপারেটর গুলোকে নিম্নোক্ত ভাগে ভাগ করা হয়। যথা -
+// Arithmetic Operators
+// Assignment Operators
+// Comparison Operators
+// Logical Operators
+// Conditional Operators
+// Type Operator
+
+// ৮. Math.abs() এর ব্যাবহার লিখুন । 
+
+var num = -20;
+var sum = Math.abs(num);
+console.log(sum);
+
+// ৯. Math.celi()  এর ব্যাবহার লিখুন । 
+var voter = 0.00001;
+var sum =Math.ceil(voter);
+console.log(sum);
+
+
+// ১০. Math.Floor() এর ব্যাবহার লিখুন । 
+var totalmembers = 10;
+ var voter = 0.99999;
+ var sum =Math.floor(voter);
+ console.log(sum);
+
+// ১১. Math.round() এর ব্যাবহার লিখুন ।
+
+var totalmembers = 10;
+var voter = 0.5;
+var sum =Math.round(voter);
+console.log(sum);
+
+// ১২. Math.random() এর ব্যাবহার লিখুন ।
+ 
+var lotary = Math.random();
+console.log(lotary);
